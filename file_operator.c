0324966f3dd2fd3e382b4d6e4bb1def212fde864
Salah Gafoor <salahgafoor@gmail.com>
2024-11-02 02:58:00 +0000

    Module open, close and read functionality added

--- file_operator.c
+++ file_operator.c
@@ -5,3 +5,50 @@
 MODULE_LICENSE("GPL"); 
 MODULE_AUTHOR("salahgafoor"); 
 MODULE_DESCRIPTION("File operator driver in Linux");
+
+static struct proc_dir_entry *proc_entry;
+
+static ssize_t get_file_content(struct file * file_pointer, 
+                                char *user_space_buffer, 
+                                size_t count, 
+                                loff_t *offset)
+{
+    char msg[] = "Module: File Operator\n";
+    size_t len = strlen(msg);
+    int result;
+
+    if(*offset >= len)
+    {
+        return 0;
+    }
+
+    result = copy_to_user(user_space_buffer, msg, len);
+    printk("This is from file operator driver");
+    
+    *offset += len;
+    
+    return len;
+}
+struct proc_ops proc_ops_driver = {
+    .proc_read = get_file_content
+};
+
+static int opening_function (void) 
+{ 
+    proc_entry = proc_create("file_operator_driver", 
+                                    0,
+                                    NULL,
+                                    &proc_ops_driver);
+
+    printk("file_operator_driver Module has been inserted");
+    return 0;
+}
+
+static void closing_function (void) 
+{ 
+    proc_remove(proc_entry);
+    printk("file_operator_driver Module has been removed");
+}
+
+module_init(opening_function);
+module_exit(closing_function);
