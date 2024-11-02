604b845210f495c398fab8e2971571ac535ea901
Salah Gafoor <salahgafoor@gmail.com>
2024-11-02 02:18:31 +0000

    added kernel headers and module description

---
+++ file_operator.c
@@ -0,0 +1,7 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/proc_fs.h>
+
+MODULE_LICENSE("GPL"); 
+MODULE_AUTHOR("salahgafoor"); 
+MODULE_DESCRIPTION("File operator driver in Linux");
